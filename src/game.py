00a9e8933f83e7b83739f35e74e822f630ba754d
gitviini <gvinicius105@gmail.com>
2026-01-01 17:22:04 -0300

    feat: moviment

--- src/game.py
+++ src/game.py
@@ -5,9 +5,24 @@ def move_entity(entity):
     entity["pos"]["x"] += entity["vel"]["x"]
     entity["pos"]["y"] += entity["vel"]["y"]
 
-def draw_tilemap(tilemap, surface):
-    for tile in tilemap:
-        pygame.draw.rect(surface, tile["color"], (tile["pos"]["x"], tile["pos"]["y"], tile["len"]["x"], tile["len"]["y"]))
+def draw_tilemap(node_tilemap, init_pos, square_size, surface):
+    tilemap = []
+    for y in range(len(node_tilemap)):
+        for x in range(len(node_tilemap[0])):
+            node = node_tilemap[y][x]
+            node_pos = {"x": init_pos["x"] + x * square_size,"y": init_pos["y"] + y * square_size}
+            tile = {
+                    "pos": node_pos,
+                    "len": {"x": square_size, "y": square_size},
+                    "color": (0, 0, 0),
+                    "collision": {"is_collide":False,"type":None},
+                }
+            if(node == 1):
+                tile["color"] = (0,0,255)
+                tile["collision"]["type"] = "bearer"
+            pygame.draw.rect(surface, tile["color"], (node_pos["x"], node_pos["y"], square_size, square_size))
+            tilemap.append(tile)
+    return tilemap
 
 def draw_entities(entities, surface):
     for entity in entities:
@@ -15,15 +30,28 @@ def draw_entities(entities, surface):
 
 def is_collision(target, obstacles):
     is_target_collide = False
-    tgt_pos = {"x": target["pos"]["x"] + target["vel"]["x"], "y": target["pos"]["y"] + target["vel"]["y"]}
-    tgt_area = {"x": target["pos"]["x"] + tgt_pos["x"], "y": target["pos"]["y"] + tgt_pos["y"]}
+    tgt_pos = {"x": target["pos"]["x"], "y": target["pos"]["y"]}
+    tgt_area = {"x": target["len"]["x"] + tgt_pos["x"], "y": target["len"]["y"] + tgt_pos["y"]}
 
+    target["collision"]["type"] = None
     for obstacle in obstacles:
         obs_pos = {"x": obstacle["pos"]["x"], "y": obstacle["pos"]["y"]}
         obs_area = {"x": obstacle["pos"]["x"] + obstacle["len"]["x"], "y": obstacle["pos"]["y"] + obstacle["len"]["y"]}
 
-        if(tgt_area["x"] >= obs_pos["x"] and tgt_pos["x"] <= obs_area["x"] and
-           tgt_area["y"] >= obs_pos["y"] and tgt_pos["y"] <= obs_area["y"]):
-            is_target_collide = True
-        
+        obstacle["collision"]["is_collide"] = False
+        if(not (tgt_area["x"] >= obs_pos["x"] and tgt_pos["x"] <= obs_area["x"] and
+           tgt_area["y"] >= obs_pos["y"] and tgt_pos["y"] <= obs_area["y"])):
+            continue
+
+        if(obstacle["collision"]["type"] == "bearer"):
+           if(target["pos"]["x"] == obs_area["x"] or target["pos"]["x"] + target["len"]["x"] == obs_pos["x"]):
+               target["vel"]["x"] = 0
+           elif(target["pos"]["y"] == obs_area["y"] or target["pos"]["y"] + target["len"]["y"] == obs_pos["y"]):
+               target["vel"]["y"] = 0
+
+        is_target_collide = True
+        target["collision"]["type"] = obstacle["collision"]["type"]
+        obstacle["collision"]["is_collide"] = True
+
+    target["collision"]["is_collide"] = is_target_collide
     return is_target_collide
--- src/main.py
+++ src/main.py
@@ -17,41 +17,40 @@ def draw_rect(surface, entity):
 
 
 def handle_move(key, entity):
-    if key == pygame.K_RIGHT:
+    if key in [pygame.K_RIGHT, pygame.K_d]:
         entity["vel"]["x"] = 1
         entity["vel"]["y"] = 0
-    elif key == pygame.K_LEFT:
+    elif key in [pygame.K_LEFT, pygame.K_a]:
         entity["vel"]["x"] = -1
         entity["vel"]["y"] = 0
-    elif key == pygame.K_UP:
+    elif key in [pygame.K_UP, pygame.K_w]:
         entity["vel"]["y"] = -1
         entity["vel"]["x"] = 0
-    elif key == pygame.K_DOWN:
+    elif key in [pygame.K_DOWN, pygame.K_s]:
         entity["vel"]["y"] = 1
         entity["vel"]["x"] = 0
 
 
 def main():
+    tmp_player = {"vel":{"x":0,"y":0}}
     player = {
-        "pos": {"x": 20, "y": 0},
+        "pos": {"x": 40, "y": 0},
         "len": {"x": 20, "y": 20},
         "vel": {"x": 0, "y": 0},
         "color": (255, 255, 0),
-        "is_collide": False,
+        "collision": {"is_collide":False,"type":None},
     }
-    tilemap = [
-        {
-            "pos": {"x": 40, "y": 40},
-            "len": {"x": 20, "y": 20},
-            "color": (0, 0, 255),
-            "is_collide": False,
-        }
+    node_tilemap = [
+    [1,0,1],
+    [1,0,0],
+    [1,1,1],
     ]
+    tilemap = []
     key_list = []
     _display_surf = None
     size = weight, height = 640, 400
     _running = True
-    square_size = 5
+    square_size = 20
 
     pygame.init()
     clock = pygame.time.Clock()
@@ -63,21 +62,22 @@ def main():
                 _running = False
             if event.type == pygame.KEYDOWN:
                 key_list.append(event.key)
+                for key in key_list:
+                    handle_move(key=key, entity=tmp_player)
             if event.type == pygame.KEYUP:
                 key_list.remove(event.key)
 
         _display_surf.fill((0, 0, 0))
-        draw_tilemap(surface=_display_surf, tilemap=tilemap)
+        tilemap = draw_tilemap(surface=_display_surf, node_tilemap=node_tilemap, square_size=square_size, init_pos={"x":40, "y": 40})
+        
+        if(player["pos"]["x"] % square_size == 0 and player["pos"]["y"] % square_size == 0):
+            player["vel"]["x"] = tmp_player["vel"]["x"]
+            player["vel"]["y"] = tmp_player["vel"]["y"]    
+        is_collision(player, tilemap)
+        
+        move_entity(player)
+        print(player["pos"])
 
-        for key in key_list:
-            handle_move(key=key, entity=player)
-        if(not is_collision(player, tilemap)):
-            move_entity(player)
-        else:
-            player["vel"]["x"] = 0
-            player["vel"]["y"] = 0
-
-        print(is_collision(player, tilemap))
         draw_rect(_display_surf, player)
         pygame.display.update()
 
